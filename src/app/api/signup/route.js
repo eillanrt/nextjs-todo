01320c4b35a21cacd963dcbcf8275df000fd5d27
Allan Robert Tan <argt@duck.com>
2023-08-04 23:52:48 +0800

    respond with savedUSer

--- src/app/api/signup/route.js
+++ src/app/api/signup/route.js
@@ -54,6 +54,7 @@ export async function POST(request) {
     return NextResponse.json(
       {
         message: 'User created',
+        savedUser,
         success: true,
       },
       { status: 201 }
