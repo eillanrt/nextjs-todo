7757b95d35ca8319dacc00d0f5785ab880ba3a2c
Allan Robert Tan <argt@duck.com>
2023-08-04 14:31:34 +0800

    handle user error

--- src/app/api/login/route.js
+++ src/app/api/login/route.js
@@ -7,38 +7,47 @@ import jwt from 'jsonwebtoken'
 connectDB()
 
 export async function POST(request) {
-  const reqBody = await request.json()
-  const { email, password } = reqBody
+  try {
+    const { email, password } = await request.json()
 
-  const user = await User.findOne({ email })
-  if (!user) {
-    return NextResponse.json(
-      { error: 'Account does not exist' },
-      { status: 400 }
-    )
+    const user = await User.findOne({ email })
+    if (!user) {
+      throw new Error('Error 400', { cause: 'Account does not exist' })
+    }
+
+    const passwordIsCorrect = await bcrypt.compare(password, user.password)
+
+    if (!passwordIsCorrect) {
+      throw new Error('Error 400', { cause: 'Wrong password' })
+    }
+
+    const tokenData = {
+      id: user._id,
+      name: user.name,
+      email: user.email,
+    }
+
+    const token = jwt.sign(tokenData, process.env.SECRET_TOKEN, {
+      expiresIn: '1d',
+    })
+
+    const response = NextResponse.json({
+      message: 'Login successfully',
+      success: true,
+    })
+
+    response.cookies.set('token', token, { httpOnly: true })
+    return response
+  } catch (error) {
+    // If error is from the user side
+    const userErrorPattern = /^Error (\d{3})/
+
+    if (userErrorPattern.test(error.message)) {
+      const errStatus = Number(error.message.match(userErrorPattern)[1])
+
+      return NextResponse.json({ error: error.cause }, { status: errStatus })
+    }
+
+    return Response.json({ error: error.message }, { status: 500 })
   }
-
-  const passwordIsCorrect = await bcrypt.compare(password, user.password)
-
-  if (!passwordIsCorrect) {
-    return NextResponse.json({ error: 'Wrong password' }, { status: 400 })
-  }
-
-  const tokenData = {
-    id: user._id,
-    name: user.name,
-    email: user.email,
-  }
-
-  const token = jwt.sign(tokenData, process.env.SECRET_TOKEN, {
-    expiresIn: '1d',
-  })
-
-  const response = NextResponse.json({
-    message: 'Login successfully',
-    success: true,
-  })
-
-  response.cookies.set('token', token, { httpOnly: true })
-  return response
 }
