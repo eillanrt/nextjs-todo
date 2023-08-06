de81931df4fc55bff26959b6729b5b11e274b237
Allan Robert Tan <argt@duck.com>
2023-08-06 21:25:50 +0800

    add handlesubmt

--- src/app/forgotpassword/page.jsx
+++ src/app/forgotpassword/page.jsx
@@ -1,19 +1,50 @@
 'use client'
+import axios from 'axios'
 import Link from 'next/link'
+import { useRef, useState } from 'react'
+import { toast } from 'react-hot-toast'
 
 export default function ForgotPasswordPage() {
+  const submitBtnRef = useRef()
+  const [email, setEmail] = useState('')
+
+  const submitEmail = (e) => {
+    e.preventDefault()
+
+    toast.promise(axios.post('/api/forgotpassword/generate', { email }), {
+      loading() {
+        submitBtnRef.current.disabled = true
+        return <b>Loading...</b>
+      },
+      success(response) {
+        console.log(response.data)
+        submitBtnRef.current.disabled = false
+        return <b>{response.data.message}</b>
+      },
+      error(err) {
+        console.log(err)
+        submitBtnRef.current.disabled = false
+        return <b>{err.response.data.error}</b>
+      },
+    })
+  }
+
   return (
     <div className="forgot-password-wrapper">
-      <form>
+      <form onSubmit={submitEmail}>
         <p>Enter the email address linked to your account</p>
         <label htmlFor="email">Email</label>
         <input
           type="email"
           name="email"
           id="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value)
+          }}
           placeholder="Email address"
         />
-        <button type="submit" className="resetpasssword-btn">
+        <button ref={submitBtnRef} type="submit" className="resetpasssword-btn">
           Reset password
         </button>
       </form>
--- src/emailBody/forgotPasswordEmailBody.js
+++ src/emailBody/forgotPasswordEmailBody.js
@@ -1,1 +1,50 @@
-export function forgotPasswordEmailBody(user) {}
+export function forgotPasswordEmailBody(user) {
+  return `<!DOCTYPE html>
+  <html>
+  <head>
+      <title>Password Reset</title>
+      <style>
+          body {
+              font-family: Arial, sans-serif;
+              line-height: 1.6;
+          }
+          .container {
+              max-width: 600px;
+              margin: 0 auto;
+              padding: 20px;
+              border: 1px solid #ccc;
+              border-radius: 5px;
+          }
+          .logo {
+              text-align: center;
+          }
+          .link {
+              display: block;
+              width: 100%;
+              text-align: center;
+              margin: 20px 0;
+          }
+          .footer {
+              text-align: center;
+              margin-top: 20px;
+          }
+      </style>
+  </head>
+  <body>
+      <div class="container">
+          <div class="logo">
+              <h1>Forgot Password</h1>
+          </div>
+          <p>Dear User,</p>
+          <p>We received a request to reset your password. Click the link below to reset your password:</p>
+          <a class="link" href="YOUR_RESET_LINK">Reset Password</a>
+          <p>If you did not request a password reset, please ignore this email.</p>
+          <p>Best regards,<br>Your App Team</p>
+          <div class="footer">
+              <p>© 2023 Your App. All rights reserved.</p>
+          </div>
+      </div>
+  </body>
+  </html>
+  `
+}
