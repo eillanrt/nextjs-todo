e6ba7bb9c39afa9e0848d6fefa4bcc5cee29a08a
Allan Robert Tan <argt@duck.com>
2023-08-06 23:52:46 +0800

    improve validatetoken route

---
+++ src/app/api/forgotpassword/validate/[token]/route.js
@@ -0,0 +1,38 @@
+import { NextResponse } from 'next/server'
+import { User } from '@/models/User'
+import { connectDB } from '@/connectDB'
+
+connectDB()
+
+export async function GET(request, { params }) {
+  try {
+    const token = params.token
+
+    const user = await User.findOne({
+      forgotPasswordToken: token,
+      forgotPasswordTokenExpiry: { $gt: Date.now() },
+    })
+
+    if (!user) {
+      throw new Error('Error 400', { cause: 'Invalid token' })
+    }
+
+    return NextResponse.json({
+      message: 'Token is valid',
+      success: true,
+      tokenIsValid: true,
+      userId: user._id,
+    })
+  } catch (error) {
+    // If error is from the user side
+    const userErrorPattern = /^Error (4\d{2})/
+
+    if (userErrorPattern.test(error.message)) {
+      const errStatus = Number(error.message.match(userErrorPattern)[1])
+
+      return NextResponse.json({ error: error.cause }, { status: errStatus })
+    }
+
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+}
