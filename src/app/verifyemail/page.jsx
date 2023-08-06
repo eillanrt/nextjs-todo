c68e1519725cda7a97196af388a20de4ff220796
Allan Robert Tan <argt@duck.com>
2023-08-06 15:15:52 +0800

    handle verifyemail

--- src/app/verifyemail/page.jsx
+++ src/app/verifyemail/page.jsx
@@ -1,16 +1,30 @@
 'use client'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import axios from 'axios'
-import { useEffect, useState, Suspense } from 'react'
+import toast, { Toaster } from 'react-hot-toast'
 
 export default function VerifyEmailPage({ searchParams }) {
   useEffect(() => {
-    const verifyToken = async () => {
-      const token = searchParams.token
-      const response = await axios.patch('/api/account/verifyemail/' + token)
-      console.log(response)
-    }
-    verifyToken()
+    toast.promise(
+      axios.patch('/api/account/verifyemail/' + searchParams.token),
+      {
+        loading: <b>Loading...</b>,
+        success(response) {
+          return <b>{response.data.message}</b>
+        },
+        error(err) {
+          return <b>{err.response.data.error}</b>
+        },
+      }
+    )
   })
 
-  return <div></div>
+  return (
+    <div>
+      <div>
+        <Toaster />
+      </div>
+    </div>
+  )
 }
