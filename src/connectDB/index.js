d01ec58f01435b3d81f0b6ad572cf737a7b3b9c0
Allan Robert Tan <argt@duck.com>
2023-08-07 10:21:52 +0800

    log uri

--- src/connectDB/index.js
+++ src/connectDB/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 export async function connectDB() {
   try {
     mongoose.connect(process.env.MONGODB_URI)
+    console.log(process.env.MONGODB_URI)
 
     const connection = mongoose.connection
 
