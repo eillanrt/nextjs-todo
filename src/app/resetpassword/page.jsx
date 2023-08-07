090267e202fd72e1331ad82075ecf988527d13da
Allan Robert Tan <argt@duck.com>
2023-08-07 09:21:40 +0800

    improve "resetpassword"

--- src/app/resetpassword/page.jsx
+++ src/app/resetpassword/page.jsx
@@ -1,21 +1,25 @@
 'use client'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
-import { toast } from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 
 export default function ResetPasswordPage({ searchParams }) {
   const router = useRouter()
+  const submitBtnRef = useRef()
   const [passwords, setPasswords] = useState({
     currentPassword: '',
     newPassword: '',
   })
   const [token, setToken] = useState('')
   const [showNewPassword, setShowNewPasword] = useState(false)
-  const [showConfirmNewPassword, setShowConfirmNewPassword] = useState(false)
+  const [showConfirmNewPassword, setShowConfirmPassword] = useState(false)
   const [tokenIsValid, setTokenIsValid] = useState('looding')
 
   useEffect(() => {
+    if (!searchParams.token) {
+      setTokenIsValid(false)
+    }
     setToken(searchParams.token)
 
     const validateToken = async () => {
@@ -24,7 +28,6 @@ export default function ResetPasswordPage({ searchParams }) {
           '/api/forgotpassword/validate/' + token
         )
 
-        console.log(response.data)
         setTokenIsValid(true)
       } catch (err) {
         setTokenIsValid(false)
@@ -38,74 +41,105 @@ export default function ResetPasswordPage({ searchParams }) {
   const changePassword = async (e) => {
     e.preventDefault()
 
-    try {
-      const response = await axios.patch('/api/forgotpassword/resetpassword', {
+    toast.promise(
+      axios.patch('/api/forgotpassword/resetpassword', {
         ...passwords,
         token,
-      })
-      console.log(response.data)
-    } catch (err) {
-      console.log(err)
-    } finally {
-      router.push('/login')
-    }
+      }),
+      {
+        loading() {
+          submitBtnRef.current.disabled = true
+          return <b>Loading...</b>
+        },
+        success() {
+          router.push('/login')
+          return <b>Password changed successfully</b>
+        },
+        error(err) {
+          submitBtnRef.current.disabled = false
+          return <b>{err.response.data.error}</b>
+        },
+      }
+    )
   }
 
   let toRender = <></>
 
   if (tokenIsValid === false) {
-    toRender = (
-      <div>
-        <h1>Invalid token</h1>
-      </div>
-    )
+    toRender = <h1>Invalid token</h1>
   } else if (tokenIsValid === 'loading') {
     toRender = <h1>Loading...</h1>
   } else {
     toRender = (
-      <div>
-        <form onSubmit={changePassword}>
-          <div>
-            <label htmlFor="new-password">New Password</label>
-            <input
-              type={showNewPassword ? 'text' : 'password'}
-              id="new-password"
-              placeholder="Enter new password"
-              value={passwords.currentPassword}
-              onChange={(e) => {
-                setPasswords({ ...passwords, currentPassword: e.target.value })
-              }}
-              required
-            />
-          </div>
-          <div>
-            <label htmlFor="confirm-new-password">Confirm Password</label>
-            <input
-              type={showConfirmNewPassword ? 'text' : 'password'}
-              id="confirm-new-password"
-              placeholder="Confirm new password"
-              value={passwords.newPassword}
-              onChange={(e) => {
-                setPasswords({ ...passwords, newPassword: e.target.value })
-              }}
-              required
-            />
-          </div>
-          <div>
-            <button
-              type="submit"
-              className="submit-reset-passwors"
-              disabled={
-                passwords.currentPassword === '' || passwords.newPassword === ''
-              }
-            >
-              Change
-            </button>
-          </div>
-        </form>
-      </div>
+      <form onSubmit={changePassword}>
+        <div>
+          <label htmlFor="new-password">New Password</label>
+          <br />
+          <input
+            type={showNewPassword ? 'text' : 'password'}
+            id="new-password"
+            placeholder="Enter new password"
+            value={passwords.currentPassword}
+            onChange={(e) => {
+              setPasswords({ ...passwords, currentPassword: e.target.value })
+            }}
+            required
+          />
+          <br />
+          <input
+            type="checkbox"
+            id="show-new-password"
+            onChange={(e) => {
+              setShowNewPasword(e.target.checked)
+            }}
+          />
+          <label htmlFor="show-new-password">Show password</label>
+        </div>
+        <div>
+          <label htmlFor="confirm-new-password">Confirm Password</label>
+          <br />
+          <input
+            type={showConfirmNewPassword ? 'text' : 'password'}
+            id="confirm-new-password"
+            placeholder="Confirm new password"
+            value={passwords.newPassword}
+            onChange={(e) => {
+              setPasswords({ ...passwords, newPassword: e.target.value })
+            }}
+            required
+          />
+          <br />
+          <input
+            type="checkbox"
+            id="show-confirm-password"
+            onChange={(e) => {
+              setShowConfirmPassword(e.target.checked)
+            }}
+          />
+          <label htmlFor="show-confirm-password">Show confirm password</label>
+        </div>
+        <div>
+          <button
+            ref={submitBtnRef}
+            type="submit"
+            className="submit-reset-password"
+            disabled={
+              passwords.currentPassword === '' || passwords.newPassword === ''
+            }
+          >
+            Change
+          </button>
+        </div>
+      </form>
     )
   }
 
-  return toRender
+  return (
+    <div className="resetpassword-wrapper">
+      <div>
+        <Toaster />
+      </div>
+      {toRender}
+    </div>
+  )
 }
