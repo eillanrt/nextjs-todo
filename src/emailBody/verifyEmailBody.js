bd1d4b4896071c85814ac7b125d769631fa42a9f
Allan Robert Tan <argt@duck.com>
2023-08-10 21:20:27 +0800

    removed classes and use inline css;

--- src/emailBody/verifyEmailBody.js
+++ src/emailBody/verifyEmailBody.js
@@ -1,16 +1,20 @@
-export function verifyEmailBody(user) {
+export function verifyEmailBody({ name }) {
   const link = process.env.BASE_URL + `/verifyemail?token=${user.verifyToken}`
 
   return `
-  <div>
-    <h1>Hello, ${user.name}</h1>
-    <div class="link">
-      <p>Note that the link below is only valid for 15 mins</p>
-      <a href="${link}">Verify Email Link</a>
+  <div style="font-family: Arial, sans-serif; background-color: #f1f1f1;">
+    <h1 style="color: #4285f4;">Hello, ${name}</h1>
+    <div style="background-color: #fff; padding: 20px; border-radius: 5px; box-shadow: 0 0 5px rgba(0, 0, 0, 0.3); max-width: 400px; margin: 20px auto;">
+        <p style="color: #333;">Note that the link below is only valid for 15 mins</p>
+        <a style="display: inline-block; padding: 10px 20px; background-color: #4285f4; color: #fff; text-decoration: none; border-radius: 3px;" href="${link}">Verify Email</a>
     </div>
     <p>If the link above doesn't work, try copy and pasting the link below in your browser to verify your email:</p>
-    <p class="expiry-note">Note that the link is only valid for 15 minutes</p>
-    <p class="expiry-link">${link}</p>
+    <p style="margin-top: 10px; color: #999;">Note that the link is only valid for 15 minutes</p>
+    <p style="word-break: break-all;">${link}</p>
+    <div style="text-align: center; margin-top: 20px">
+      <p>© ${new Date().getFullYear()} Allan Robert Tan. All rights reserved.</p>
   </div>
+</div>
+
     `
 }
