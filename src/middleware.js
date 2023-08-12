944f22bdde2f59426301a8fc3f20608c149da471
Allan Robert Tan <argt@duck.com>
2023-08-12 13:54:37 +0800

    Revert "handle invalid token on middleware"
    This reverts commit 184ae1a1159258b790abfd9efe4a11f8e148e016.

--- src/middleware.js
+++ src/middleware.js
@@ -1,23 +1,9 @@
 import { NextResponse } from 'next/server'
-import { getUserIdFromToken } from './getUserIdFromToken'
 
 export function middleware(request) {
   const path = request.nextUrl.pathname
+  const token = request.cookies.get('token')?.value || ''
   const publicPaths = ['/login', '/signup', '/forgotpassword', '/resetpassword']
-
-  let token = request.cookies.get('token')?.value || ''
-  let resetToken = false
-
-  // Make sure the token is valid before proceeding
-  if (token) {
-    try {
-      getUserIdFromToken(token)
-    } catch (error) {
-      // in case token is invalid, reset it
-      token = ''
-      resetToken = true
-    }
-  }
 
   const isPublicPath = publicPaths.includes(path)
 
@@ -26,15 +12,7 @@ export function middleware(request) {
   }
 
   if (!isPublicPath && !token) {
-    const response = NextResponse.redirect(new URL('/login', request.nextUrl))
-
-    if (resetToken) {
-      response.cookies.set('token', '', {
-        httpOnly: true,
-        expires: new Date(0),
-      })
-    }
-    return response
+    return NextResponse.redirect(new URL('/login', request.nextUrl))
   }
 }
 
