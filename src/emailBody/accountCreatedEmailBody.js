e6b5ec424640903915adc2d5a4f7964b391152dc
Allan Robert Tan <argt@duck.com>
2023-08-13 14:11:12 +0800

    Create an email body for revoke accoun

---
+++ src/emailBody/accountCreatedEmailBody.js
@@ -0,0 +1,11 @@
+export function accountCreatedEmailBody({ name, unlinkEmailToken }) {
+  const link = process.env.BASE_URL + `/revokeaccount?token=${unlinkEmailToken}`
+
+  return `Welcome ${name}
+<div>
+An account was created on ${process.env.BASE_URL} using this email address. <br/>
+If you did not create an account using this email address, you can delete the account linked with email address by going through this link:
+<p>${link}</p> 
+</div>
+`
+}
